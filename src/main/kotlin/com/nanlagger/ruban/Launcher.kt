fe058b01f8b21a285c4065d261eb6990df529aaa
Stepan Lyashenko <lyashenko_se@npmgroup.ru>
2016-09-07 12:38:55 +0700

    lab 1 compete

--- src/main/kotlin/com/nanlagger/ruban/Launcher.kt
+++ src/main/kotlin/com/nanlagger/ruban/Launcher.kt
@@ -6,6 +6,41 @@ import com.nanlagger.ruban.labs.Lab1
  * Created by admin on 04.09.2016.
  */
 fun main(args: Array<String>) {
-    val f = {x: Float -> 4*x*x + 2*x + 4}
-    println(String.format("%.2f", Lab1.searchMin(f, -10f, 10f, 0.001f)))
+    val f = {x: Float -> x*x - 2*x - 4}
+    val f2 = {x: Float, y: Float -> 4*x*x+5*x+3*x*y+4*y+y*y}
+    var res: Float
+    var res2: Pair<Float, Float>
+
+    TimeTracker.start()
+    res = Lab1.searchMin(f, -10f, 10f, 0.000001f)
+    TimeTracker.print()
+    println(res)
+
+    TimeTracker.start()
+    res = Lab1.searchMinUpgrade(f, -10f, 10f, 0.000001f)
+    TimeTracker.print()
+    println(res)
+
+    TimeTracker.start()
+    res2 = Lab1.searchMin(f2, -10f, 10f, 0.000001f)
+    TimeTracker.print()
+    println(res2)
+
+    TimeTracker.start()
+    res2 = Lab1.searchMinUpgrade(f2, -10f, 10f, 0.000001f)
+    TimeTracker.print()
+    println(res2)
+}
+
+object TimeTracker {
+    private var startTime: Long = System.nanoTime()
+
+    fun print() {
+        val time = System.nanoTime() - startTime
+        println("Время выполнения: ${time / 1000000} ms ${time % 1000000} ns")
+    }
+
+    fun start() {
+        startTime = System.nanoTime()
+    }
 }
--- src/main/kotlin/com/nanlagger/ruban/labs/Lab1.kt
+++ src/main/kotlin/com/nanlagger/ruban/labs/Lab1.kt
@@ -6,7 +6,7 @@ package com.nanlagger.ruban.labs
 object Lab1 {
     val fi = (1f + Math.sqrt(5.0).toFloat()) / 2f
 
-    fun searchMin(f: (Float) -> Float, left: Float, right: Float, e: Float): Float {
+    fun searchMinUpgrade(f: (Float) -> Float, left: Float, right: Float, e: Float): Float {
         var a = left
         var b = right
         var x1 = b - (b - a) / fi
@@ -23,5 +23,58 @@ object Lab1 {
             }
         }
         return (a + b) / 2
+    }
+
+    fun searchMin(f: (Float) -> Float, left: Float, right: Float, e: Float): Float {
+        var a = left
+        var b = right
+        while (Math.abs(b - a) > e) {
+            val x1 = a + (b - a) / 3
+            val x2 = b - (b - a) / 3
+            if(f(x1) >= f(x2)) {
+                a = x1
+            } else {
+                b = x2
+            }
+        }
+        return (a + b) / 2
+    }
+
+    fun searchMin(f: (Float, Float) -> Float, left: Float, right: Float, e: Float): Pair<Float, Float> {
+        var a = left
+        var b = right
+        while (Math.abs(b - a) > e) {
+            val x1 = a + (b - a) / 3
+            val x2 = b - (b - a) / 3
+            val y1 = searchMin({y: Float -> f(x1, y)}, left, right, e)
+            val y2 = searchMin({y: Float -> f(x2, y)}, left, right, e)
+            if(f(x1, y1) >= f(x2, y2)) {
+                a = x1
+            } else {
+                b = x2
+            }
+        }
+        return Pair((a + b) / 2, searchMin({y: Float -> f((a + b) / 2, y)}, left, right, e))
+    }
+
+    fun searchMinUpgrade(f: (Float, Float) -> Float, left: Float, right: Float, e: Float): Pair<Float, Float> {
+        var a = left
+        var b = right
+        var x1 = b - (b - a) / fi
+        var x2 = a + (b - a) / fi
+        while (Math.abs(b - a) > e) {
+            val y1 = searchMinUpgrade({y: Float -> f(x1, y)}, left, right, e)
+            val y2 = searchMinUpgrade({y: Float -> f(x2, y)}, left, right, e)
+            if(f(x1, y1) >= f(x2, y2)) {
+                a = x1
+                x1 = x2
+                x2 = a + (b - a) / fi
+            } else {
+                b = x2
+                x2 = x1
+                x1 = b - (b - a) / fi
+            }
+        }
+        return Pair((a + b) / 2, searchMin({y: Float -> f((a + b) / 2, y)}, left, right, e))
     }
 }
